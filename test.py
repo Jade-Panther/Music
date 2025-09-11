b2d054eded291416620784b90a97e001b6f0b25c
Squiddy Kitty <iostreamplus@proton.me>
2025-09-11 18:15:39 +0000

    Update

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 import csv
 import cs50
 import requests
+import time
 
 db = cs50.SQL('sqlite:///songs.db')
 
@@ -17,8 +18,10 @@ with open('new.csv') as file:
             title = line[0]
             author = line[1].replace('\n', '')
             try:
+                y = 10/0
                 lyrics = requests.get(f'https://api.lyrics.ovh/v1/{author}/{title}').json()['lyrics']
                 lyrics = lyrics.replace('\n\n', '\n').replace('\n', '<br>')
+                
             except Exception as e:
                 lyrics = 'Error getting lyrics'
             
@@ -27,4 +30,5 @@ with open('new.csv') as file:
             author = clean(author)
             lyrics = clean(lyrics)
 
+            print('Inserting ' + title + ' by ' + author + ' with ID ' + videoId)
             db.execute('INSERT INTO songs (video_id, title, author, lyrics) VALUES (?, ?, ?, ?);', videoId, title, author, lyrics)
